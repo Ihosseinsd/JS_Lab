5b4ff157b894c1e6a375532ac72854ebb6566815
Hossein Sadeghi <141355464+Ihosseinsd@users.noreply.github.com>
2024-04-26 13:41:03 +0330

    Delete nextsession3/3.js

--- nextsession3/3.js
+++
@@ -1,6 +0,0 @@
-
-function selectFun() {
-    const img1 = document.querySelector(".img1");
-    const tmp = document.getElementById("temp");
-    tmp.setAttribute("src", img1.setAttribute("src"))
-}

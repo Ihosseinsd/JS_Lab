512cbf729bc97ea1bd4808d40bfb38914884c756
Hossein Sadeghi <141355464+Ihosseinsd@users.noreply.github.com>
2024-04-26 13:36:28 +0330

    Add files via upload

---
+++ nextsession3/3.js
@@ -0,0 +1,6 @@
+
+function selectFun() {
+    const img1 = document.querySelector(".img1");
+    const tmp = document.getElementById("temp");
+    tmp.setAttribute("src", img1.setAttribute("src"))
+}
